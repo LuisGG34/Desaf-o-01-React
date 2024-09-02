b0dfc9041d8845cf74ea77918a4c39ef7be91bcb
LuisGG34 <luis.gutierrezgo@mail.udp.cl>
2024-09-02 17:00:32 -0400

    subir repositorio

--- desafio01/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/Desaf-o-01-React",
-  plugins: [react()],
-})
